842fc94cb1c0aaf7b2722fb1a8475955b90b92d7
Devan Mardyks <xmready@protonmail.com>
2022-04-08 16:23:28 -0700

    Added ycmcompile.sh
    Added first iteration of ycmcompile.sh. This is a base for future changes.

---
+++ ycmcompile.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+#compile YouCompleteMe with all language features
+echo -e "\n$(tput setaf 3)compiling ycm\n$(tput sgr0)" \
+&& cd ~/.vim/plugged/YouCompleteMe \
+&& python3 install.py --clangd-completer --cs-completer --go-completer --ts-completer --rust-completer --java-completer \
+&& echo -e "\n$(tput setaf 2)ycm compiled\n$(tput sgr0)"
