786c83dabd32b3a87328ac9a1495e989e4b7f8d9
Devan Mardyks <xmready@protonmail.com>
2022-04-08 15:52:31 -0700

    Added vim-setup.sh
    Added first iteration of vim-setup.sh. This is a base for future changes.

---
+++ vim-setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+echo -e "\n$(tput setaf 3)installing vim and dependencies\n$(tput sgr0)" \
+&& sudo apt update \
+&& sudo apt install wget curl vim-nox python3-dev mono-complete golang nodejs default-jdk npm \
+&& echo -e "\n$(tput setaf 2)packages installed\n$(tput sgr0)" \
+&& sleep 3 \
+
+&& echo -e "\n$(tput setaf 3)adding vimrc, vim-plug, templates\n$(tput sgr0)" \
+&& curl -fLo ~/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc \
+&& sudo curl -fLo /root/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root \
+&& curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+&& curl -fLo ~/.vim/templates/skeleton.html --create-dirs https://raw.githubusercontent.com/xmready/vim-config/main/.vim/templates/skeleton.html \
+&& mkdir ~/.vim/swap \
+&& echo -e "\n$(tput setaf 2)vimrc, vim-plug, templates added\n$(tput sgr0)"
