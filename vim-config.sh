6b72c8e92a55c76bfd1fa2cc6ce142d83e258f78
xmready <xmready@protonmail.com>
2022-09-30 12:15:11 -0700

    fix: vim-config.sh installation procedure

--- vim-config.sh
+++ vim-config.sh
@@ -9,6 +9,32 @@
 # Non-root usage:
 #   curl -fsSL https://raw.githubusercontent.com/xmready/vim-config/main/vim-config.sh | bash -
 
+# vim-nox installation and configuration
+&& echo -e "\n$(tput setaf 3)installing Vim\n$(tput sgr0)" \
+&& sudo apt update \
+&& sudo apt install curl build-essential cmake golang mono-complete openjdk-17-jdk openjdk-17-jre python3-dev vim-nox \
+&& echo -e "\n$(tput setaf 2)Vim installed\n$(tput sgr0)" \
+&& sleep 3 \
+&& echo -e "\n$(tput setaf 3)configuring Vim\n$(tput sgr0)" \
+# create Vim directories
+&& mkdir -p ~/.vim/swap \
+&& mkdir -p ~/.vim/autoload \
+&& mkdir -p ~/.vim/templates/html \
+&& mkdir -p ~/.vim/templates/js \
+&& mkdir -p ~/.vim/templates/ts \
+# add .vimrc
+&& curl -fLo ~/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc \
+&& sudo curl -fLo /root/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root \
+# add vim-plug
+&& curl -fLo ~/.vim/autoload/plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
+# add template files
+&& curl -fLo ~/.vim/templates/html/skeleton.html https://raw.githubusercontent.com/xmready/vim-config/main/.vim/templates/skeleton.html \
+# add scripts to /usr/local/bin
+&& sudo curl -fLo /usr/local/bin/ycmcompile https://raw.githubusercontent.com/xmready/vim-config/main/ycmcompile.sh \
+&& sudo chmod +x /usr/local/bin/ycmcompile \
+&& echo -e "\n$(tput setaf 2)Vim configured\n$(tput sgr0)"
+&& sleep 3 \
+
 # NodeSource repository configuration and installation
 echo -e "\n$(tput setaf 3)adding NodeSource repo\n$(tput sgr0)" \
 && KEYRING=/usr/share/keyrings/nodesource.gpg \
@@ -23,28 +49,3 @@ echo -e "\n$(tput setaf 3)installing nodejs\n$(tput sgr0)" \
 && sudo apt update \
 && sudo apt install -y nodejs \
 && echo -e "\n$(tput setaf 2)nodejs installed\n$(tput sgr0)" \
-&& sleep 3 \
-
-# install vim-nox and dependencies
-&& echo -e "\n$(tput setaf 3)installing Vim and dependencies\n$(tput sgr0)" \
-&& sudo apt update \
-&& sudo apt install curl build-essential cmake golang mono-complete openjdk-17-jdk openjdk-17-jre python3-dev vim-nox \
-&& echo -e "\n$(tput setaf 2)packages installed\n$(tput sgr0)" \
-&& sleep 3 \
-
-# add vimrc
-&& echo -e "\n$(tput setaf 3)adding vimrc, vim-plug, templates\n$(tput sgr0)" \
-&& mkdir -p ~/.vim/swap \
-&& curl -fLo ~/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc \
-&& sudo curl -fLo /root/.vimrc https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root \
-
-# add vim-plug
-&& curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim \
-
-# add skeleton files
-&& curl -fLo ~/.vim/templates/skeleton.html --create-dirs https://raw.githubusercontent.com/xmready/vim-config/main/.vim/templates/skeleton.html \
-
-# add ycmcompile.sh
-&& sudo curl -fLo /usr/local/bin/ycmcompile https://raw.githubusercontent.com/xmready/vim-config/main/ycmcompile.sh \
-&& sudo chmod +x /usr/local/bin/ycmcompile \
-&& echo -e "\n$(tput setaf 2)vimrc, vim-plug, templates added\n$(tput sgr0)"
