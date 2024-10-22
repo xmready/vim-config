8b5125901eb14f5608a032de6927570712756a0c
xmready <77082140+xmready@users.noreply.github.com>
2024-10-21 20:13:06 -0700

    Update setup-vim.sh
    updated to use nvm instead of nodesource

--- setup-vim.sh
+++ setup-vim.sh
@@ -9,46 +9,35 @@
 # Non-root usage:
 #   wget -O- https://raw.githubusercontent.com/xmready/vim-config/main/setup-vim.sh | bash -
 
-NODE_PGP=https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key
-NODE_KEYRING=/usr/share/keyrings/nodesource.gpg
-NODE_URL=https://deb.nodesource.com
-
-get_latest_release_major() {
-  curl --silent "https://api.github.com/repos/$1/releases/latest" |
-  grep tag_name |
-  sed -E 's/.*v([[:digit:]]+).*/\1/'
-}
-
-NODE_LATEST="$(get_latest_release_major nodejs/node)"
-NODE_SOURCES=/etc/apt/sources.list.d/nodesource.list
-YCM_COMPILE=https://raw.githubusercontent.com/xmready/vim-config/main/ycm-compile.sh
-GIT_URL=https://github.com/xmready/vim-config.git
-PLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 get_latest_release() {
   curl --silent "https://api.github.com/repos/$1/releases/latest" |
   grep tag_name |
   sed -E 's/.*"([^"]+)".*/\1/'
 }
 
+NVM_LATEST="$(get_latest_release nvm-sh/nvm)"
+NVM_URL=https://raw.githubusercontent.com/nvm-sh/nvm/"$NVM_LATEST"/install.sh
+YCM_COMPILE=https://raw.githubusercontent.com/xmready/vim-config/main/ycm-compile.sh
+GIT_URL=https://github.com/xmready/vim-config.git
+PLUG_URL=https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 NERD_LATEST="$(get_latest_release ryanoasis/nerd-fonts)"
 DEJA_URL=https://github.com/ryanoasis/nerd-fonts/releases/download/"$NERD_LATEST"/DejaVuSansMono.tar.xz
 FIRA_URL=https://github.com/ryanoasis/nerd-fonts/releases/download/"$NERD_LATEST"/FiraCode.tar.xz
 HACK_URL=https://github.com/ryanoasis/nerd-fonts/releases/download/"$NERD_LATEST"/Hack.tar.xz
 JET_URL=https://github.com/ryanoasis/nerd-fonts/releases/download/"$NERD_LATEST"/JetBrainsMono.tar.xz
 
-echo -e "\n$(tput setaf 3)adding NodeSource repo\n$(tput sgr0)" \
-&& curl -fL "$NODE_PGP" \
-  | gpg --dearmor \
-  | sudo tee "$NODE_KEYRING" > /dev/null \
-&& echo "deb [signed-by=$NODE_KEYRING] $NODE_URL/node_$NODE_LATEST.x nodistro main" \
-  | sudo tee "$NODE_SOURCES" \
-&& echo -e "\n$(tput setaf 2)NodeSource repo added\n$(tput sgr0)" \
+echo -e "\n$(tput setaf 3)installing Nodejs\n$(tput sgr0)" \
+&& curl -fL "$NVM_URL" \
+  | bash \
+&& nvm install node \
+&& node -v \
+&& npm -v \
+&& echo -e "\n$(tput setaf 2)Nodejs installed\n$(tput sgr0)" \
 && sleep 3 \
 && echo -e "\n$(tput setaf 3)installing Vim\n$(tput sgr0)" \
 && sudo apt update \
 && sudo apt install -y \
-  curl build-essential cmake golang mono-complete nodejs \
+  curl build-essential cmake golang mono-complete \
   openjdk-17-jdk openjdk-17-jre python3-dev vim-nox \
 && echo -e "\n$(tput setaf 2)Vim installed\n$(tput sgr0)" \
 && sleep 3 \
