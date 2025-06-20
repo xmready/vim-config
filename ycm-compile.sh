091de0ba9c1c774f4b310f81f49a51eda4951298
xmready <77082140+xmready@users.noreply.github.com>
2025-06-20 13:55:00 -0700

    Update ycm-compile.sh

--- ycm-compile.sh
+++ ycm-compile.sh
@@ -4,5 +4,10 @@
 echo -e "\n$(tput setaf 3)compiling ycm\n$(tput sgr0)" \
 && cd ~/.vim/plugged/YouCompleteMe \
 && python3 install.py \
-  --clangd-completer --cs-completer --go-completer --ts-completer --rust-completer --java-completer \
+  --clangd-completer \
+  --cs-completer \
+  --go-completer \
+  --ts-completer \
+  --rust-completer \
+  --java-completer \
 && echo -e "\n$(tput setaf 2)ycm compiled\n$(tput sgr0)"
