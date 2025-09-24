7164b0dbf3624bd79671a09d2a8708908b96a5c5
xmready <77082140+xmready@users.noreply.github.com>
2025-09-24 12:52:41 -0700

    Add setup script for custom Vim server configuration
    This script downloads and configures a custom .vimrc for both the user and root.

---
+++ setup-server-vim.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+# Discussion, issues and change requests at:
+#   https://github.com/xmready/system-setup
+#
+# Purpose:
+#   A script to create custom .vimrc for servers
+#
+# sudo usage:
+#   curl -fL https://raw.githubusercontent.com/xmready/vim-config/main/setup-server-vim.sh | bash -
+
+VIMRC_ROOT_URL=https://raw.githubusercontent.com/xmready/vim-config/main/.vimrc-root
+
+echo -e "\n$(tput setaf 3)configuring Vim\n$(tput sgr0)" \
+&& curl -fLo ~/.vimrc "$VIMRC_ROOT_URL" \
+&& sudo curl -fLo /root/.vimrc "$VIMRC_ROOT_URL" \
+&& sleep 3 \
+&& sudo -v \
+&& echo -e "\n$(tput setaf 2)Vim configured\n$(tput sgr0)"
